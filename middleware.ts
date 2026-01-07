3522ba6920eb498820c929818202630cad3d67d1
nitin bhatt <nitinbhatt9536@gmail.com>
2026-01-07 21:32:36 +0530

    project completed

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute=createRouteMatcher([
+const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
   '/summaires(.*)',
   '/upload(.*)'
