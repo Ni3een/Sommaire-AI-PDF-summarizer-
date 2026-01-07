0014060a3af7dc26ed58c5daabe477e8e08ac22d
nitin bhatt <nitinbhatt9536@gmail.com>
2026-01-07 23:23:54 +0530

    done:

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -3,4 +3,7 @@ import {createRouteHandler} from "uploadthing/next";
 
 export const {GET,POST}=createRouteHandler({
     router:outFileRouter,
+    config: {
+        callbackUrl: process.env.UPLOADTHING_URL ? `${process.env.UPLOADTHING_URL}/api/uploadthing` : undefined,
+    }
 })
